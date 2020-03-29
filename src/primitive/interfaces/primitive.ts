254d30271f40c304499876ba594078ee803f85ce
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-29 18:39:27 +0800

    feat: added an assertion function for primitive values

---
+++ src/primitive/interfaces/primitive.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Representing a primitive object.
+ * 
+ * @remarks
+ * Primitive means the payload is not an `Object` and has no methods.
+ * 
+ * @remarks
+ * The primitive types JavaScript currently have were:
+ * 
+ * - `bigint`
+ * - `boolean`
+ * - `null`
+ * - `number`
+ * - `string`
+ * - `symbol`
+ * - `undefined`
+ * 
+ * @since 0.0.1
+ */
+export type IPrimitive = bigint | boolean | null | number | string | symbol | undefined;
---
+++ src/primitive/is-primitive.ts
@@ -0,0 +1,47 @@
+"use strict";
+
+import { isNull } from "../is-null";
+import { isUndefined } from "../is-undefined";
+
+import { isBigInt } from "../bigint/is-bigint";
+import { isBoolean } from "../boolean/is-boolean";
+import { isNumber } from "../number/is-number";
+import { isString } from "../string/is-string";
+import { isSymbol } from "../symbol/is-symbol";
+
+import { IPrimitive } from "./interfaces/primitive";
+
+/**
+ * Determine whether the payload is a primitive value or not.
+ * 
+ * @remarks
+ * Primitive means the payload is not an `Object` and has no methods.
+ * 
+ * @remarks
+ * The primitive types JavaScript currently have were:
+ * 
+ * - `bigint`
+ * - `boolean`
+ * - `null`
+ * - `number`
+ * - `string`
+ * - `symbol`
+ * - `undefined`
+ * 
+ * @since 0.0.1
+ * 
+ * @param payload An object to be determined.
+ * @return The assertion result.
+ */
+export function isPrimitive(payload: unknown): payload is IPrimitive
+{
+    return (
+        isBigInt(payload)    ||
+        isBoolean(payload)   ||
+        isNull(payload)      ||
+        isNumber(payload)    ||
+        isString(payload)    ||
+        isSymbol(payload)    ||
+        isUndefined(payload)
+    );
+}
