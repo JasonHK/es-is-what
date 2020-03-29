86d6afc8753d61a9df97b0291390e1017801810b
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-29 19:28:25 +0800

    chore: added "index.ts"

---
+++ src/number/index.ts
@@ -0,0 +1,8 @@
+"use strict";
+
+export * from "./is-any-number";
+export * from "./is-any-number-like";
+export * from "./is-nan";
+export * from "./is-nan-like";
+export * from "./is-number";
+export * from "./is-number-like";
