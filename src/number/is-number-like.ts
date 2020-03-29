b46d1022be34358c827b33dce156b46849d3ea56
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-29 18:06:07 +0800

    feat: added an assertion function for number-like object

---
+++ src/number/is-number-like.ts
@@ -0,0 +1,34 @@
+"use strict";
+
+import { TAG_NUMBER } from "../internal/string-tags";
+
+import { hasToStringTagOf } from "../object/utilities/has-to-string-tag-of";
+
+import { isNumber } from "./is-number";
+
+/**
+ * Determine whether the payload is a number-like object or not.
+ * 
+ * @remarks
+ * Number-like means the payload is either a number or an object wrapped number primitive.
+ * 
+ * @remarks
+ * All valid numbers (including `Infinity` and `-Infinity`) will be count as a number.
+ * 
+ * @since 0.0.1
+ * 
+ * @param payload An object to be determined.
+ * @returns The assertion result.
+ */
+export function isSymbolLike(payload: unknown): payload is symbol | Symbol
+{
+    return (
+        isNumber(payload) ||
+        (
+            hasToStringTagOf<Number>(payload, TAG_NUMBER) &&
+            isNumber(payload.valueOf())
+        )
+    );
+}
+
+export default isSymbolLike;
