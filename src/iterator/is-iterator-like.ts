f81118313546639b58ec7efa65470bdfdfcadaa9
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-30 16:57:30 +0800

    refactor: refactored `isIteratorLike`

--- src/iterator/is-iterator-like.ts
+++ src/iterator/is-iterator-like.ts
@@ -3,11 +3,12 @@
 import { isAnyFunction } from "../function/is-any-function";
 import { isAnyObject } from "../object/is-any-object";
 
-import { IObject } from "../object/interfaces/object";
-
 import { IIteratorLike } from "./interfaces/iterator-like";
 
 export function isIteratorLike<T extends IIteratorLike = IIteratorLike>(payload: unknown): payload is T
 {
-    return (isAnyObject<IObject>(payload) && isAnyFunction(payload.next));
+    return (
+        isAnyObject<T>(payload) &&
+        (Reflect.has(payload, "next") && isAnyFunction(payload.next))
+    );
 }
