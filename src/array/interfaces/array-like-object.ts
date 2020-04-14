067cc9fd697f566697575566e19c05d5ae827a96
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-14 15:15:45 +0800

    docs: added TSDoc

--- src/array/interfaces/array-like-object.ts
+++ src/array/interfaces/array-like-object.ts
@@ -2,4 +2,13 @@
 
 import { IArrayLike } from "./array-like";
 
+/**
+ * Representing an array-like object.
+ * 
+ * @remarks
+ * Array-like means the payload is not a function and has the `length` property which is an integer
+ * between `0` and `MAX_SAFE_INTEGER` (inclusive).
+ * 
+ * @since 0.0.1
+ */
 export type IArrayLikeObject = object & IArrayLike;
--- src/array/interfaces/array-like.ts
+++ src/array/interfaces/array-like.ts
@@ -1,3 +1,12 @@
 "use strict";
 
+/**
+ * Representing an array-like value.
+ * 
+ * @remarks
+ * Array-like means the payload is not a function and has the `length` property which is an integer
+ * between `0` and `MAX_SAFE_INTEGER` (inclusive).
+ * 
+ * @since 0.0.1
+ */
 export type IArrayLike = ArrayLike<unknown>;
