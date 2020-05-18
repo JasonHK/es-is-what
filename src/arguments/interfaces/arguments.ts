1cb6d83b680ac4d8328c42d2057c6ae097f41e9d
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-18 21:42:06 +0800

    docs: added TSDoc

--- src/arguments/interfaces/arguments.ts
+++ src/arguments/interfaces/arguments.ts
@@ -1,1 +1,5 @@
+/**
+ * Representing the `arguments` object accessible inside functions that contains the values of the
+ * arguments passed to that function.
+ */
 export type IArguments = globalThis.IArguments;
