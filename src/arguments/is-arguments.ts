59f6edba8c31ac9b3e4cf7db106d4a347865dc4a
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-18 22:03:43 +0800

    docs: added notes

--- src/arguments/is-arguments.ts
+++ src/arguments/is-arguments.ts
@@ -13,6 +13,10 @@ import type { IArguments } from "./interfaces/arguments";
 /**
  * Determine whether the payload is the `arguments` object or not.
  * 
+ * @notes
+ * The `arguments` object is an array-like object accessible inside functions that contains the
+ * values of the arguments passed to that function.
+ * 
  * @since 0.0.1
  * 
  * @param payload An value to be determined.
