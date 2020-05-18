5a31fe6cd6142af5408bf8b155950724eeb8501f
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-18 22:00:31 +0800

    docs: added TSDoc

--- src/arguments/is-arguments.ts
+++ src/arguments/is-arguments.ts
@@ -10,6 +10,25 @@ import { hasToStringTagOf } from "../object/utilities/has-to-string-tag-of";
 
 import type { IArguments } from "./interfaces/arguments";
 
+/**
+ * Determine whether the payload is the `arguments` object or not.
+ * 
+ * @since 0.0.1
+ * 
+ * @param payload An value to be determined.
+ * @returns The assertion result.
+ * 
+ * @example
+ * ```typescript
+ * isArguments(getArguments());
+ * // > true
+ * 
+ * isArguments([]);
+ * // > false
+ * 
+ * function getArguments() { return arguments; }
+ * ```
+ */
 export function isArguments(payload: unknown): payload is IArguments
 {
     return (
