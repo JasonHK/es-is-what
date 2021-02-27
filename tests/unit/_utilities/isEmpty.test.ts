e328bf3af9cf9707948f300c2aaf4883f3f3b4e2
Jason Kwok <JasonHK@users.noreply.github.com>
2021-02-27 22:49:14 +0800

    test: added more test cases

--- tests/unit/_utilities/isEmpty.test.ts
+++ tests/unit/_utilities/isEmpty.test.ts
@@ -9,9 +9,64 @@ describe(nameof(isEmpty), () =>
             expect(isEmpty("")).toBeTrue();
         });
 
-        test("when the payload is an empty Array", () =>
+        test("when the payload is an empty String instance", () =>
+        {
+            expect(isEmpty(new String(""))).toBeTrue();
+        });
+
+        test("when the payload is an empty Array instance", () =>
         {
             expect(isEmpty([])).toBeTrue();
+        });
+
+        test("when the payload is an empty Array-like object", () =>
+        {
+            expect(isEmpty({ length: 0 })).toBeTrue();
+        });
+
+        test("when the payload is an empty Map instance", () =>
+        {
+            expect(isEmpty(new Map())).toBeTrue();
+        });
+
+        test("when the payload is an empty Set instance", () =>
+        {
+            expect(isEmpty(new Set())).toBeTrue();
+        });
+
+        test("when the payload is an empty ArrayBuffer instance", () =>
+        {
+            expect(isEmpty(new ArrayBuffer(0))).toBeTrue();
+        });
+
+        test("when the payload is an empty SharedArrayBuffer instance", () =>
+        {
+            expect(isEmpty(new SharedArrayBuffer(0))).toBeTrue();
+        });
+
+        test("when the payload is an empty DataView instance", () =>
+        {
+            expect(isEmpty(new DataView(new ArrayBuffer(0)))).toBeTrue();
+        });
+
+        test("when the payload is an empty Iterable value", () =>
+        {
+            const iterable = {
+                [Symbol.iterator]()
+                {
+                    return {
+                        next()
+                        {
+                            return {
+                                value: undefined,
+                                done: true,
+                            };
+                        },
+                    };
+                },
+            };
+
+            expect(isEmpty(iterable)).toBeTrue();
         });
     });
 });
