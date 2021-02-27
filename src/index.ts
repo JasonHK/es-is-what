a5def22d021c26259ba9ec97198b5fe61e5608df
Jason Kwok <JasonHK@users.noreply.github.com>
2021-02-27 23:13:23 +0800

    feat: added root "index.ts"

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,80 @@
-"use strict";
+// ECMAScript Language Types
+// https://tc39.es/ecma262/#sec-ecmascript-language-types
 
-export * from "./primitive";
-export * from "./bigint";
-export * from "./boolean";
-export * from "./null";
-export * from "./number";
-export * from "./string";
-export * from "./symbol";
-export * from "./undefined";
+export * from "./Undefined";
+export * from "./Null";
+export * from "./Boolean";
+export * from "./String";
+export * from "./Symbol";
+export * from "./Number";
+export * from "./BigInt";
+export * from "./Object";
 
-export * from "./object";
-export * from "./arguments";
-export * from "./array";
-export * from "./array-buffer";
-export * from "./data-view";
-export * from "./descriptor";
-export * from "./function";
-export * from "./iterable";
-export * from "./iterator";
-export * from "./map";
-export * from "./promise";
-export * from "./reg-exp";
-export * from "./set";
-export * from "./typed-array";
+// Fundamental Objects
+// https://tc39.es/ecma262/#sec-fundamental-objects
+//
+// NOTE: `Object`, `Boolean` and `Symbol` were already exported.
 
-export * from "./utilities";
+export * from "./Function";
+export * from "./Error";
+
+// Numbers and Dates
+// https://tc39.es/ecma262/#sec-numbers-and-dates
+//
+// NOTE: `Number` and `BigInt` were already exported.
+
+export * from "./Date";
+
+// Text Processing
+// https://tc39.es/ecma262/#sec-text-processing
+//
+// NOTE: `String` was already exported.
+
+export * from "./RegExp";
+
+// Indexed Collections
+// https://tc39.es/ecma262/#sec-indexed-collections
+
+export * from "./Array";
+export * from "./TypedArray";
+
+// Keyed Collections
+// https://tc39.es/ecma262/#sec-keyed-collections
+
+export * from "./Map";
+export * from "./Set";
+export * from "./WeakMap";
+export * from "./WeakSet";
+
+// Structured Data
+// https://tc39.es/ecma262/#sec-structured-data
+
+export * from "./ArrayBuffer";
+export * from "./SharedArrayBuffer";
+export * from "./DataView";
+
+// Managing Memory
+// https://tc39.es/ecma262/#sec-managing-memory
+
+export * from "./WeakRef";
+export * from "./FinalizationRegistry";
+
+// Control Abstraction Objects
+// https://tc39.es/ecma262/#sec-control-abstraction-objects
+
+export * from "./Iterable";
+export * from "./Iterator";
+export * from "./Promise";
+
+// Built-in Exotic Objects
+// https://tc39.es/ecma262/#sec-built-in-exotic-object-internal-methods-and-slots
+
+export * from "./Arguments";
+
+// Miscellaneous
+
+export * from "./Primitive";
+
+// Utility or Helper Functions
+
+export * from "./_utilities";
