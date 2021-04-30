5a18b9123704588947780cd85a9d50a5716864ad
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-30 16:17:47 +0800

    fix: removed the `Promise` handlers

--- src/Promise/isPromise.ts
+++ src/Promise/isPromise.ts
@@ -2,10 +2,8 @@ import callBound from "call-bind/callBound";
 
 const $then = callBound("%Promise.prototype.then%");
 
-const noop = () => {};
-
 export default function isPromise<T extends Promise<unknown> = Promise<unknown>>(payload: unknown): payload is T
 {
-    try { $then(payload, noop, noop); } catch { return false; }
+    try { $then(payload); } catch { return false; }
     return true;
 }
