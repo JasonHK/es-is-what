ca452e117a0f04860ec0da4a2e697833b9c0813d
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-30 16:34:26 +0800

    chore: renamed internal type

--- src/Promise/isPromiseLike.ts
+++ src/Promise/isPromiseLike.ts
@@ -3,7 +3,7 @@ import { SetOptional } from "type-fest";
 import isObject from "../Object/isObject";
 import isCallable from "../Function/isCallable";
 
-type T = SetOptional<PromiseLike<unknown>>;
+type MaybePromiseLike = SetOptional<PromiseLike<unknown>>;
 
 /**
  * Determine whether the payload is a `Promise`-like object.
@@ -15,5 +15,5 @@ type T = SetOptional<PromiseLike<unknown>>;
  */
 export default function isPromiseLike(payload: unknown): payload is PromiseLike<unknown>
 {
-    return (isObject<T>(payload) && isCallable(payload.then));
+    return (isObject<MaybePromiseLike>(payload) && isCallable(payload.then));
 }
