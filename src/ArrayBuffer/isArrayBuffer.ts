7e3479c1640b1419002e577dd8eca43014c80dbf
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-30 16:32:54 +0800

    docs: added TSDoc

--- src/ArrayBuffer/isArrayBuffer.ts
+++ src/ArrayBuffer/isArrayBuffer.ts
@@ -1,19 +1,17 @@
 import callBound from "call-bind/callBound";
 
-/**
- * The call-bound version of the [`ArrayBuffer.prototype.byteLength`](https://tc39.es/ecma262/#sec-get-arraybuffer.prototype.bytelength)
- * get accessor.
- * 
- * Used to check for the existence of the `[[ArrayBufferData]]` internal slot, and the result of
- * [`IsSharedArrayBuffer(obj)`](https://tc39.es/ecma262/#sec-issharedarraybuffer) abstract operation.
- */
 const $byteLength = callBound("%ArrayBuffer.prototype.byteLength%");
 
+/**
+ * Determine whether the payload is an `ArrayBuffer` object.
+ *
+ * @since 0.0.1
+ *
+ * @param payload The value to determine.
+ * @returns The assertion result.
+ */
 export default function isArrayBuffer(payload: unknown): payload is ArrayBuffer
 {
-    // Check for the existence of the `[[ArrayBufferData]]` internal slot, and the result of
-    // [`IsSharedArrayBuffer(obj)`] abstract operation.
     try { $byteLength(payload as ArrayBuffer); } catch { return false; }
-
     return true;
 }
--- src/SharedArrayBuffer/isSharedArrayBuffer.ts
+++ src/SharedArrayBuffer/isSharedArrayBuffer.ts
@@ -4,15 +4,29 @@ import noopFalse from "../_helpers/noopFalse";
 
 interface IsSharedArrayBuffer
 {
+    /**
+     * Determine whether the payload is an `SharedArrayBuffer` object.
+     *
+     * @since 0.0.1
+     *
+     * @param payload The value to determine.
+     * @returns The assertion result.
+     */
     (payload: unknown): payload is SharedArrayBuffer;
 }
 
 const $byteLength = callBound("%SharedArrayBuffer.prototype.byteLength%", true);
 
+/**
+ * Determine whether the payload is an `SharedArrayBuffer` object.
+ *
+ * @since 0.0.1
+ */
 let isSharedArrayBuffer: IsSharedArrayBuffer;
+
 if ($byteLength)
 {
-    isSharedArrayBuffer = ((payload): boolean =>
+    isSharedArrayBuffer = (function isSharedArrayBuffer(payload): boolean
     {
         try { $byteLength(payload as SharedArrayBuffer); } catch { return false; }
         return true;
