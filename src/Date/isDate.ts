259d6fe2cc2aec9bc583a89e6021130ee0784da5
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-30 15:54:24 +0800

    docs: updated TSDoc

--- src/Date/isDate.ts
+++ src/Date/isDate.ts
@@ -1,28 +1,22 @@
 import callBound from "call-bind/callBound";
 
-/**
- * The call-bound version of the [`Date.prototype.valueOf`](https://tc39.es/ecma262/#sec-date.prototype.valueof)
- * function. Used to check for the existence of the `[[DateValue]]` internal slot.
- */
 const $valueOf = callBound("%Date.prototype.valueOf%");
 
 /**
- * Determine whether the payload is a `Date` object or not.
+ * Determine whether the payload is a `Date` object.
  *
  * @since 0.0.1
  *
- * @param payload An value to be determined.
+ * @param payload The value to determine.
  * @returns The assertion result.
  *
  * @example
- * ```typescript
+ * ```ts
  * isDate(new Date());
  * // > true
  *
  * isDate(Date());
  * // > false
- * //
- * // NOTE: Calling `Date()` without the `new` operator returns a string.
  *
  * isDate(Date.now());
  * // > false
@@ -30,8 +24,6 @@ const $valueOf = callBound("%Date.prototype.valueOf%");
  */
 export default function isDate(payload: unknown): payload is Date
 {
-    // Check for the existence of the `[[DateValue]]` internal slot.
     try { $valueOf(payload); } catch { return false; }
-
     return true;
 }
