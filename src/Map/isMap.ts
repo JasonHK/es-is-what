2d6b0c7b98bf34d2019277367ec3c20618e088a9
Jason Kwok <JasonHK@users.noreply.github.com>
2021-05-28 16:05:52 +0800

    docs: added TSDoc

--- src/Map/isMap.ts
+++ src/Map/isMap.ts
@@ -1,12 +1,22 @@
 import callBound from "call-bind/callBound";
 
-// Used as the key when calling `Map.prototype.has`.
+/**
+ * Used as the key when calling `Map.prototype.has`.
+ */
 const MARKER = {};
 
+/**
+ * Returns a boolean indicating whether an element with the specified key exists or not.
+ * 
+ * An error will be thrown if `this` did not have the `[[MapData]]` internal slot.
+ * 
+ * @see https://tc39.es/ecma262/#sec-map.prototype.has
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/has
+ */
 const $has = callBound("%Map.prototype.has%");
 
 /**
- * Determine whether the payload is a `Map` object.
+ * Determine whether the payload is a `Map` instance.
  *
  * @since 0.0.1
  * 
